7de9595305187c8f3fe030043758011c3f377892
metherton <martinetherton63@googlemail.com>
2014-12-11 23:17:14 +0100

    added census

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,16 +4,60 @@
 
 var onsControllers = angular.module('onsControllers', ['ui.grid', 'ui.grid.pagination']);
 
-onsControllers.controller('PersonListCtrl', ['$scope', 'personService', '$routeParams', '$location', '$route',
-    function($scope, personService, $routeParams, $location, $route) {
+onsControllers.controller('PersonListCtrl', ['$scope', 'personService', '$routeParams', '$location', '$route', '$modal', '$log',
+    function($scope, personService, $routeParams, $location, $route, $modal, $log) {
 
-        $scope.personsForm = personService.query();
+        $scope.gridOptions = {};
+        $scope.gridOptions.onRegisterApi = function (gridApi) {
+            $scope.gridApi = gridApi;
+        };
 
-        $scope.orderProp = 'person.birthDate';
+        personService.query().$promise.then(function(data) {
+                $scope.surnames = data.surnames;
+                $scope.fathers = data.fatherDetails;
+                $scope.mothers = data.motherDetails;
+                $scope.gridOptions.data = data.personDetails;
+            }
+        );
+
+        $scope.gridOptions.columnDefs = [
+            { field: 'person.firstName', displayName: 'First Name'},
+            { field: 'person.surname.surname', displayName: 'Surname'},
+            { field: 'birthDate', displayName: 'Date Of Birth'},
+            { field: 'fatherDetails', displayName: 'Father'},
+            { field: 'motherDetails', displayName: 'Mother'}
+        ];
+
 
         $scope.addPerson = function(person) {
             personService.addPerson(person).$promise.then($route.reload);
             $scope.personDetails = {};
+        };
+
+        $scope.open = function (size) {
+
+            var modalInstance = $modal.open({
+                resolve: {
+                    surnames: function() {
+                        return $scope.surnames;
+                    },
+                    fathers: function() {
+                        return $scope.fathers;
+                    },
+                    mothers: function() {
+                        return $scope.mothers;
+                    }
+                },
+                templateUrl: 'addPersonForm.html',
+                controller: 'AddPersonCtrl',
+                size: size
+            });
+
+            modalInstance.result.then(function (person) {
+                personService.addPerson(person).$promise.then($route.reload);
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
         };
 
     }
@@ -42,16 +86,138 @@ onsControllers.controller('AddSurnameCtrl', function ($scope, $modalInstance) {
     };
 });
 
-onsControllers.controller('LocationListCtrl', ['$scope', 'locationService', '$routeParams', '$location', '$route',
-    function($scope, locationService, $routeParams, $location, $route) {
+onsControllers.controller('AddLocationCtrl', function ($scope, $modalInstance, countries) {
 
-        $scope.locationsForm = locationService.query();
+    $scope.countries = countries;
+
+    $scope.change = function() {
+        console.log('changed');
+    }
+
+    $scope.isEmpty = function(value) {
+        return value === undefined;
+    }
+
+    $scope.ok = function () {
+        $modalInstance.close($scope.location);
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+});
+
+onsControllers.controller('AddPersonCtrl', function ($scope, $modalInstance, surnames, fathers, mothers) {
+
+    $scope.surnames = surnames;
+    $scope.fathers = fathers;
+    $scope.mothers = mothers;
+
+    $scope.person = {};
+
+    $scope.change = function() {
+        console.log('changed');
+    }
+
+    $scope.isEmpty = function(value) {
+        return value === undefined;
+    }
+
+    $scope.ok = function () {
+        $modalInstance.close($scope.person);
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+
+    $scope.today = function() {
+        $scope.person.birthDate = new Date();
+    };
+    $scope.today();
+
+    $scope.clear = function () {
+        $scope.person.birthDate = null;
+    };
+
+    // Disable weekend selection
+    $scope.disabled = function(date, mode) {
+        return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+    };
+
+    $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.opened = true;
+    };
+
+    $scope.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
+
+
+
+
+
+
+});
+
+
+
+onsControllers.controller('LocationListCtrl', ['$scope', 'locationService', '$routeParams', '$location', '$route', '$modal', '$log',
+    function($scope, locationService, $routeParams, $location, $route, $modal, $log) {
+
+        $scope.gridOptions = {};
+        $scope.gridOptions.onRegisterApi = function (gridApi) {
+            $scope.gridApi = gridApi;
+        };
+
+        locationService.query().$promise.then(function(data) {
+                $scope.countries = data.countries;
+                $scope.gridOptions.data = data.locations;
+            }
+        );
+
+        $scope.gridOptions.columnDefs = [
+            { field: 'addressLine1', displayName: 'Address Line 1'},
+            { field: 'addressLine2', displayName: 'Address Line 2'},
+            { field: 'city', displayName: 'City'},
+            { field: 'postCode', displayName: 'Post Code'},
+            { field: 'country.name', displayName: 'Country'}
+        ];
+
+
+    //    $scope.locationsForm = locationService.query();
 
         $scope.addLocation = function(location) {
             locationService.addLocation(location).$promise.then($route.reload);
             $scope.location = {};
         };
 
+        $scope.open = function (size) {
+
+            var modalInstance = $modal.open({
+                resolve: {
+                    countries: function() {
+                        return $scope.countries;
+                    }
+                },
+                templateUrl: 'addLocationForm.html',
+                controller: 'AddLocationCtrl',
+                size: size
+            });
+
+            modalInstance.result.then(function (location) {
+                locationService.addLocation(location).$promise.then($route.reload);
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
     }
 ]);
 
